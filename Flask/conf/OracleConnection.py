163c098a67df2a136da8e1568d7fa4926f33ac09
Jose-godoy-mendoza <jgodoym11@miumg.edu.gt>
2023-10-14 00:24:32 -0600

    cruds

--- Flask/conf/OracleConnection.py
+++ Flask/conf/OracleConnection.py
@@ -1,4 +1,6 @@
 import cx_Oracle
+from datetime import datetime
+
 
 #try:
 #    OracleConnection = cx_Oracle.connect(
@@ -18,6 +20,10 @@ try:
 except Exception as e:
     print(f"Error al conectar a Oracle: {str(e)}")
 
-#Oracle_Connection = OracleConnection.cursor()
+Oracle_Connection = OracleConnection.cursor()
 #Oracle_Connection.execute("insert into Bancos values (14, 'asdfasfsd')")
+#OracleConnection.commit()
+#Current_Date = datetime.now().strftime("%d%m%Y")
+#print(Current_Date)
+#Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - 10 WHERE idcuenta = 102030")
 #OracleConnection.commit()
--- Flask/main.py
+++ Flask/main.py
@@ -22,6 +22,14 @@ class insert_transaction(BaseModel):
     Remitente: str
     Mensaje: str
 
+class Delete_Transactions(BaseModel):
+    IdTransaccion: str
+
+
+class Insert_Bank(BaseModel):
+    IdBank:str
+    BankName:str
+
 app = FastAPI()
 
 
@@ -69,7 +77,7 @@ def Login(Username:str, Password:str):
 @app.get("/list")
 def list():
     Oracle_Connection = OracleConnection.cursor()
-    Bank_List = Oracle_Connection.execute("select * from bancos").fetchall()
+    Bank_List = Oracle_Connection.execute("select * from bancos ORDER BY IdBanco").fetchall()
     #print(Bank_List)
     HTMLString=""
     DropDown_Banks = ""
@@ -89,22 +97,241 @@ def list():
     else:
         Oracle_Connection.close()
         return Response(status_code=HTTP_400_BAD_REQUEST)
+    
+@app.post("/api/insert_bank/<IdBank><BankName>", response_model=Insert_Bank)
+def Insert_Bank(IdBank:str, BankName:str):
+    try:
+        Oracle_Connection = OracleConnection.cursor()
+        Oracle_Connection.execute("insert into Bancos values ("+IdBank+", '"+BankName+"')")
+        OracleConnection.commit()
+    except cx_Oracle.DatabaseError as e:
+        print("Error al ejecutar la consulta de inserción:", e)
+    return {"IdBank":IdBank, "BankName":BankName}
+
+
+@app.delete("/api/delete_Bancos/<IdBanco>")
+def Delete_Bank(IdBanco:str):
+    try:
+        Oracle_Connection = OracleConnection.cursor()
+        Oracle_Connection.execute("delete from Bancos where IdBanco= '"+IdBanco+"'")
+        OracleConnection.commit()
+    except cx_Oracle.DatabaseError as e:
+        print("Error al ejecutar la consulta de delete:", e)
+
+
+@app.post("/api/update_bancos/<IdBanco><Nombre>")
+def Update_Bank(IdBanco:str, Nombre:str):
+    try:
+        Oracle_Connection = OracleConnection.cursor()
+        Oracle_Connection.execute("update bancos set nombre = '"+Nombre+"' where idbanco= '"+IdBanco+"'")
+        OracleConnection.commit()
+    except cx_Oracle.DatabaseError as e:
+        print("Error al ejecutar la consulta de delete:", e)
+
 
 @app.post("/api/insert/<Beneficiario><Banco_Beneficiario><Monto><Remitente><Mensaje>", response_model=insert_transaction)
 def list(Beneficiario: str, Banco_Beneficiario: str, Monto: str, Remitente: str, Mensaje: str):
-    #Current_Date = datetime.now().strftime("%Y%m%d")
+    CurrentDate = datetime.now().strftime("%d%m%Y")
     try:
-        CurrentDate = "10/10/2023"
+        #CurrentDate = "10/10/2023"
         try:
             Oracle_Connection = OracleConnection.cursor()
             GetBankId = str(Oracle_Connection.execute("select idbanco from bancos where nombre='"+str(Banco_Beneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            Bank_Account = str(Oracle_Connection.execute("select idbanco from cuentas where idcuenta='"+str(Beneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetSaldoRemitente = str(Oracle_Connection.execute("select saldo from cuentas where idcuenta='"+str(Remitente)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            print("Remitente: ",GetSaldoRemitente, " enviado: ", Monto)
+            print("*-*-*-*- ",Bank_Account)
             
-            print("******----",GetBankId,"----*****")
-            Oracle_Connection.execute("insert into transacciones values ('1', '"+Monto+"', '1','"+Beneficiario+"','"+GetBankId+"','" +Remitente+"', '1','"+str(CurrentDate)+"','" +str(Mensaje)+"')")
-            OracleConnection.commit()
-            print("funciona")
+
+
+            if int(GetSaldoRemitente) >= int(Monto):
+                print("SI HAY FONDOS SUFIENTES, saldo enviado: ",Monto)
+                print("REMITENTE: ",Remitente, " monto a tansferir: ", Monto)
+                if str(Beneficiario) != str(Remitente):
+                    print("DIFERENTES CUENTAS")
+                    if Bank_Account == GetBankId:
+                        print("BANCOS COINCIDEN, APROVADO")
+                        Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+str(Monto)+" WHERE idcuenta = '"+str(Remitente)+"'")
+                        OracleConnection.commit()
+                        Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+str(Monto)+" WHERE idcuenta = '"+str(Beneficiario)+"'")
+                        OracleConnection.commit()
+                        Oracle_Connection.execute("insert into transacciones values ('1', '"+Monto+"', '1','"+Beneficiario+"','"+GetBankId+"','" +Remitente+"', '1','"+str(CurrentDate)+"','" +str(Mensaje)+"')")
+                        OracleConnection.commit()
+                    else:
+                        print("NO COINCIDEN, DENEGADO")
+                else:
+                    print("NO SE PUEDE TRANSFERIR A LA MISMA CUENTA")
+            else:
+                print("NO HAY FONDOS, saldo en cuenta: ", GetSaldoRemitente)
+                
+            
         except cx_Oracle.DatabaseError as e:
             print("Error al ejecutar la consulta de inserción:", e)
         return {"Remitente":Remitente, "Mensaje":Mensaje}
+    except ValidationError as exc:
+        print(repr(exc.errors()[0]['type']))
+
+
+@app.get("/api/show_transactions")
+def show_transactions():
+    Oracle_Connection = OracleConnection.cursor()
+    Transaction_list = Oracle_Connection.execute("""
+    SELECT transacciones.idtransaccion, tipo_transaccion.Descripcion, transacciones.Cuenta_Beneficiaria, 
+    Bancos.Nombre, transacciones.monto, transacciones.idcuenta_remitente, 
+    estados.descripcion, transacciones.comentario
+    FROM transacciones
+    INNER JOIN tipo_transaccion
+    ON transacciones.idtipo_transaccion = tipo_transaccion.idtipo_transaccion
+    INNER JOIN Bancos
+    ON transacciones.IDBANCO_BENEFICIARIO = bancos.idbanco
+    INNER JOIN estados
+    ON transacciones.IDESTADO = ESTADOS.IDESTADO ORDER BY transacciones.idtransaccion
+                                          """).fetchall()
+    
+    HTMLString=""
+    if Transaction_list is not None:
+        
+        for Trx in Transaction_list:
+            HTMLString+= "<tr>"
+            HTMLString+="<td>"+str(Trx[0])+"</td>"
+            HTMLString+="<td>"+str(Trx[1])+"</td>"
+            HTMLString+="<td>"+str(Trx[2])+"</td>"
+            HTMLString+="<td>"+str(Trx[3])+"</td>"
+            HTMLString+="<td>"+str(Trx[4])+"</td>"
+            HTMLString+="<td>"+str(Trx[5])+"</td>"
+            HTMLString+="<td>"+str(Trx[6])+"</td>"
+            HTMLString+="<td>"+str(Trx[7])+"</td>"
+            HTMLString +="<td> <a class=\"btn btn-success\">Edit</a> | <a href=\"/Delete_Transaction\" class=\"btn btn-danger\">Delete</a> </td>"
+            HTMLString +="</tr>"            
+        Oracle_Connection.close()
+        return {"Transactions_List":HTMLString}
+    else:
+        Oracle_Connection.close()
+        return Response(status_code=HTTP_400_BAD_REQUEST)
+    
+
+@app.delete("/api/trx_Delete/<IdTransaction>", response_model=Delete_Transactions)
+def Delete_Transaction(IdTransaccion:str):
+    try:
+        Oracle_Connection = OracleConnection.cursor()
+
+        GetBeneficiario = str(Oracle_Connection.execute("select Cuenta_Beneficiaria from Transacciones where IDtransaccion ='"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+        print("BENEFICIARIO: ", GetBeneficiario)
+        GetRemitente = str(Oracle_Connection.execute("select IDCUENTA_REMITENTE from Transacciones where IDtransaccion ='"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+        print("remitente: ", GetRemitente)
+        GetSaldo_Beneficiario = str(Oracle_Connection.execute("select saldo from cuentas where idcuenta='"+str(GetBeneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+        print("SALDO: ", GetSaldo_Beneficiario)
+        GetSaldo_Trx = str(Oracle_Connection.execute("select monto from transacciones where idtransaccion='"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+        print("monto de transaccion", GetSaldo_Trx)
+        
+        if int(GetSaldo_Beneficiario) >= int(GetSaldo_Trx):
+            Oracle_Connection.execute("DELETE FROM transacciones where IdTransaccion='"+str(IdTransaccion)+"'")
+            OracleConnection.commit()
+            print("COMPLETADO")
+            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+GetSaldo_Trx+" WHERE idcuenta = '"+GetBeneficiario+"'")
+            OracleConnection.commit()
+            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+GetSaldo_Trx+" WHERE idcuenta = '"+GetRemitente+"'")
+            OracleConnection.commit()
+            
+            
+            
+        else:
+            print("NO PODEMOS RETIRAR EL MONTO, CUENTA QUEDARÁ EN NEGATIVO")
+
+    except cx_Oracle.DatabaseError as e:
+        print("Error al ejecutar la consulta de delete:", e)
+    return {"IdTransaccion":IdTransaccion}
+
+
+@app.post("/api/Update/<IdTransaccion><Beneficiario><Banco_Beneficiario><Monto><Remitente><Comentario>", response_model=insert_transaction)
+def Update(IdTransaccion: str, Beneficiario: str, Banco_Beneficiario: str, Monto: str, Remitente: str, Comentario: str):
+    CurrentDate = datetime.now().strftime("%d%m%Y")
+    try:
+        try:
+            Oracle_Connection = OracleConnection.cursor()
+            GetOld_AccountBeneficiario = str(Oracle_Connection.execute("select CUENTA_BENEFICIARIA from transacciones where idtransaccion = '"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetOld_BalanceBeneficiario = str(Oracle_Connection.execute("select saldo from cuentas where idcuenta='"+str(GetOld_AccountBeneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetOld_AccountRemitente = str(Oracle_Connection.execute("select IDCUENTA_REMITENTE from transacciones where idtransaccion = '"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetOld_BalanceRemitente = str(Oracle_Connection.execute("select saldo from cuentas where idcuenta='"+str(GetOld_AccountBeneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            
+            GetSaldoRemitente = str(Oracle_Connection.execute("select saldo from cuentas where idcuenta='"+str(Remitente)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetOld_TrxAmount = str(Oracle_Connection.execute("select monto from transacciones where idtransaccion='"+str(IdTransaccion)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            
+            NewBank_AccountBeneficiario = str(Oracle_Connection.execute("select idbanco from cuentas where idcuenta='"+str(Beneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            GetNew_Bank = str(Oracle_Connection.execute("select idbanco from bancos where nombre='"+str(Banco_Beneficiario)+"'").fetchone()).replace(",","").replace("(","").replace(")","")
+            
+            
+            print("Old Account: ",GetOld_AccountBeneficiario, " Sent: ", GetOld_TrxAmount)
+            print("New Account: ",Beneficiario," new amt: ", Monto)
+            AdjustedAmt = 0
+            if int(GetOld_TrxAmount) > int(Monto):
+                AdjustedAmt = int(GetOld_TrxAmount) - int(Monto)
+            elif int(GetOld_TrxAmount) < int(Monto):
+                AdjustedAmt = int(Monto) - int(GetOld_TrxAmount)
+            else:
+                AdjustedAmt = Monto
+
+
+            if (str(Beneficiario) != str(GetOld_AccountBeneficiario)) or (str(Beneficiario) == str(GetOld_AccountBeneficiario) and GetOld_BalanceBeneficiario != AdjustedAmt):
+                print("Diferente beneficiario o diferente cantidad con mismo beneficiario")
+                if int(GetOld_BalanceBeneficiario) >= int(AdjustedAmt):
+                    print("la cuenta antigua cuenta con balance para reversar la transaccion")
+                    if NewBank_AccountBeneficiario == GetNew_Bank:
+                        print("Bancos coinciden, podemos seguir")
+                        if str(Beneficiario) != str(GetOld_AccountBeneficiario):
+                            print("Cantidades iguales o diferentes, diferente beneficiario")
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+str(Monto)+" WHERE idcuenta = '"+str(Beneficiario)+"'")
+                            OracleConnection.commit()
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+str(GetOld_TrxAmount)+" WHERE idcuenta = '"+str(GetOld_AccountBeneficiario)+"'")
+                            OracleConnection.commit()
+                        
+                        elif int(Monto) > int(GetOld_TrxAmount):
+                            print("new amount is higher that trx amt")
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+str(AdjustedAmt)+" WHERE idcuenta = '"+str(GetOld_AccountBeneficiario)+"'")
+                            OracleConnection.commit()
+                            
+                        elif int(Monto) < int(GetOld_TrxAmount):
+                            print("old trx amt mayor")
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+str(AdjustedAmt)+" WHERE idcuenta = '"+str(GetOld_AccountBeneficiario)+"'")
+                            OracleConnection.commit()
+                        
+                        else:
+                            print("nada")
+
+                        if str(GetOld_AccountRemitente) != str(Remitente):
+                            print("Diferente cuenta remitente")
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+str(GetOld_TrxAmount)+" WHERE idcuenta = '"+str(GetOld_AccountRemitente)+"'")
+                            OracleConnection.commit()
+                            Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+str(GetOld_TrxAmount)+" WHERE idcuenta = '"+str(Remitente)+"'")
+                            OracleConnection.commit()
+                        elif (str(GetOld_AccountRemitente) == str(Remitente) and GetOld_BalanceBeneficiario != AdjustedAmt):
+                            if int(Monto) > int(GetOld_TrxAmount):
+                                print("Misma cuenta pero transfiriendo mas")
+                                Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo - "+str(AdjustedAmt)+" WHERE idcuenta = '"+str(GetOld_AccountRemitente)+"'")
+                                OracleConnection.commit()
+                            elif int(Monto) < int(GetOld_TrxAmount):
+                                print("Misma cuenta pero reduciendo transferencia")
+                                Oracle_Connection.execute("UPDATE Cuentas SET saldo = saldo + "+str(AdjustedAmt)+" WHERE idcuenta = '"+str(GetOld_AccountRemitente)+"'")
+                                OracleConnection.commit()
+                            else:
+                                print("Misma cantidad")
+                        else:
+                            print("Misma Cuenta Remitente")
+                        Oracle_Connection.execute(""" 
+                        UPDATE Transacciones SET Monto ="""+str(Monto)+""", cuenta_beneficiaria = '"""+str(Beneficiario)+"""', 
+                        idbanco_beneficiario = """+str(NewBank_AccountBeneficiario)+""", idcuenta_remitente = '"""+str(Remitente)+"""', Fecha = '"""+str(CurrentDate)+"""', 
+                        Comentario='"""+str(Comentario)+"""' where idtransaccion = """+str(IdTransaccion)+"""
+                        """)
+                        OracleConnection.commit()
+                    else:
+                        print("Bancos no coinciden")
+                else:
+                    print("Balance insuficiente, no podemos reversar la transaccion")
+            else:
+                print("Mismo beneficiario")         
+            
+        except cx_Oracle.DatabaseError as e:
+            print("Error al ejecutar la consulta de inserción:", e)
+        return {"Remitente":Remitente, "Mensaje":Comentario}
     except ValidationError as exc:
         print(repr(exc.errors()[0]['type']))
